850ed52e154cbe3f54f4fca687bfa67e2c39d152
MainVolpi <raul.volpi28@gmail.com>
2025-02-15 13:16:12 -0300

    Tirei o prompt

--- site girl/main.js
+++ site girl/main.js
@@ -1,14 +1,1 @@
-const nomeAprovado = [
-    'Raul', 'raul', 'Raul Volpi', 'raul volpi',
-    'Manuela', 'manuela', 'Manuh', 'manuh',
-    'Manu', 'manu', 'Botelho', 'botelho',
-    'Manuh Botelho', 'manuh botelho', 'Manu Botelho', 'manu botelho',
-    'Manuela Botelho', 'manuela botelho'
-];
-const nome = prompt('Qual seu Nome??')
-if (nomeAprovado.includes(nome)) {
-    alert('Você não pode entrar no site!!!')
-    window.location.replace("");
-} else {
-    alert('Bem-vindo ao site!')
-}
+
